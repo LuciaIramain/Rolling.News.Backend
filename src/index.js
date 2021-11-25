c017efe232bd62b2fb50e3e2ada0a047cc278445
Iramain <agusiramain92@gmail.com>
2021-11-25 16:38:36 -0300

    Se agrego la configuracion inicial del proyecto

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+// Settings
+const app = express();
+app.set('port', process.env.PORT || 4000);
+
+app.listen(app.get('port'), () => {
+    console.log('estoy en el puerto ' + app.get('port'));
+});
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Rutas
+app.get('/', (req, res) => {
+    res.send('Su solicitud ha sido enviada');
+});
