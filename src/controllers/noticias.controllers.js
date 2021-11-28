1824cc9c56c44094c45ac7c405bb6ae6d86c5b88
Iramain <agusiramain92@gmail.com>
2021-11-28 13:13:39 -0300

    Se modifico editarNoticia en notias.controllers

--- src/controllers/noticias.controllers.js
+++ src/controllers/noticias.controllers.js
@@ -72,22 +72,10 @@ noticiaCtrl.obtenerNoticia = async (req, res) => {
 
 noticiaCtrl.editarNoticia = async (req, res) => {
     try{
-        if (req.body.tituloNoticia === '' 
-        && req.body.descripcionBreve === '' 
-        && req.body.descripcionDetallada === '' 
-        && req.body.categoria === '' 
-        && req.body.autor === '' 
-        && req.body.imagen === '' 
-        && req.body.fecha === '') {
-            res.status(500).json({
-                mensaje: 'Todos los campos son obligatorios'
-            });
-        } else {
-            await Noticia.findByIdAndUpdate(req.params.id, req.body);
-            res.status(200).json({
-                mensaje: 'Su noticia fue modificada'
-            });
-        }  
+        await Noticia.findByIdAndUpdate(req.params.id, req.body);
+        res.status(200).json({
+            mensaje: 'La noticia fue modificada'
+        });
     }catch(error){
         console.log(error);
         res.status(500).json({
