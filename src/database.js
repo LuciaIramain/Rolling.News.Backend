a67a0951347797f679961ccfba268009050f8083
Lucia Iramain <luciairamain5@gmail.com>
2021-12-16 17:48:46 -0300

    Se agrego la conexion al servicio de mongo atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://localhost:27017/rollingnews';
+// const url = 'mongodb://localhost:27017/rollingnews';
+const url = 'mongodb+srv://luciairamain:Lu20Ir21@cluster0.0lnpe.mongodb.net/rollingnews';
+
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
